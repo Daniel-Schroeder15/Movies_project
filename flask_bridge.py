7255648dc67e9d4074008af97765c6e598ac977b
Eric LaFlure <ejlaflure@gmail.com>
2020-09-01 19:42:49 -0500

    updating data cleaning and machine learning code

--- flask_bridge.py
+++ flask_bridge.py
@@ -4,17 +4,14 @@ import numpy as np
 from sqlalchemy import create_engine
 from config import sql_pw
 
-# # Create Database Engine and extract Dataset from Database
-# connection_string = f"postgres://postgres:{sql_pw}@group-c-project-db.csna2pebfhlh.us-east-2.rds.amazonaws.com:5432/postgres"
-# engine = create_engine(connection_string)
-# movies_df = pd.read_sql(sql="Movie_Data_Pre-Encoding", con=engine)
-# data = movies_df.to_dict('records')
+# def Popularity_filter():
 
 def Get_genres():
     # Create Database Engine and extract Dataset from Database
     connection_string = f"postgres://postgres:{sql_pw}@group-c-project-db.csna2pebfhlh.us-east-2.rds.amazonaws.com:5432/postgres"
     engine = create_engine(connection_string)
     movies_df = pd.read_sql(sql="Movie_Data_Pre-Encoding", con=engine)
+    # Generate genre list
     genre_list = movies_df["genre"].unique().tolist()
     return genre_list
 
@@ -23,6 +20,7 @@ def Filter_movies(genre):
     connection_string = f"postgres://postgres:{sql_pw}@group-c-project-db.csna2pebfhlh.us-east-2.rds.amazonaws.com:5432/postgres"
     engine = create_engine(connection_string)
     movies_df = pd.read_sql(sql="Movie_Data_Pre-Encoding", con=engine)
+    # Generate movie list
     filtered_movies = movies_df[movies_df["genre"] == genre]
     movie_list = filtered_movies["title"].unique().tolist()
     return movie_list    
